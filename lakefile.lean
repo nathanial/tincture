5a3e76a2c652ff364d7c8dcf33fbd841066e2549
Nathanial Hartman <nathanial.hartman@gmail.com>
2025-12-28 22:20:27 -0800

    Switch to GitHub dependencies for v0.0.1 release

--- lakefile.lean
+++ lakefile.lean
@@ -4,8 +4,8 @@ open Lake DSL
 package tincture where
   version := v!"0.1.0"
 
-require crucible from ".." / "crucible"
-require staple from ".." / "staple"
+require crucible from git "https://github.com/nathanial/crucible" @ "v0.0.1"
+require staple from git "https://github.com/nathanial/staple" @ "v0.0.1"
 
 require plausible from git
   "https://github.com/leanprover-community/plausible.git" @ "v4.26.0"
