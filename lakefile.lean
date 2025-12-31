1246bedde2071aa9196e25c937f45881fb6b94a1
Nathanial Hartman <nathanial.hartman@gmail.com>
2025-12-31 01:08:45 -0800

    Update staple to v0.0.2

--- lakefile.lean
+++ lakefile.lean
@@ -5,7 +5,7 @@ package tincture where
   version := v!"0.1.0"
 
 require crucible from git "https://github.com/nathanial/crucible" @ "v0.0.1"
-require staple from git "https://github.com/nathanial/staple" @ "v0.0.1"
+require staple from git "https://github.com/nathanial/staple" @ "v0.0.2"
 
 require plausible from git
   "https://github.com/leanprover-community/plausible.git" @ "v4.26.0"
