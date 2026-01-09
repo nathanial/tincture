9cc6ce30937745a0bb51df0ffbc01661b7f69e74
Nathanial Hartman <nathanial.hartman@gmail.com>
2026-01-08 18:17:16 -0800

    Update crucible to v0.0.7 and track lake-manifest.json

--- lakefile.lean
+++ lakefile.lean
@@ -4,7 +4,7 @@ open Lake DSL
 package tincture where
   version := v!"0.1.0"
 
-require crucible from git "https://github.com/nathanial/crucible" @ "v0.0.3"
+require crucible from git "https://github.com/nathanial/crucible" @ "v0.0.7"
 require staple from git "https://github.com/nathanial/staple" @ "v0.0.2"
 
 require plausible from git
